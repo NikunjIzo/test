afba88aeb475b78c8964182fa5585ee54bc6e0d9
NikunjIzo <nikunj@izonnet.com>
2026-01-22 17:26:21 +0530

    Update V1__create_database_objects.sql

--- db/migrations/V1__create_database_objects.sql
+++ db/migrations/V1__create_database_objects.sql
@@ -1,1 +1,22 @@
+BEGIN TRY
+    BEGIN TRAN;
+
+    IF NOT EXISTS (
+        SELECT 1 FROM sys.tables WHERE name = 'schema_migrations'
+    )
+    BEGIN
+        CREATE TABLE dbo.schema_migrations (
+            version       INT PRIMARY KEY,
+            description   NVARCHAR(255),
+            script_name   NVARCHAR(255),
+            applied_at    DATETIME DEFAULT GETDATE()
+        );
+    END
+
+    COMMIT TRAN;
+END TRY
+BEGIN CATCH
+    ROLLBACK TRAN;
+    THROW;
+END CATCH;
 
